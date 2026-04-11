92b817c9143921ea574168ce1ec7bf22e8106f7d
eugenekingfish <aojg@protonmail.ch>
2026-04-12 00:14:09 +0100

    feat: merged camera factories

--- headers/factories/camera.h
+++ headers/factories/camera.h
@@ -1,59 +1,65 @@
-#ifndef BLOSSOM_FACTORY_PERSPECTIVE_CAMERA_H
-#define BLOSSOM_FACTORY_PERSPECTIVE_CAMERA_H
+#ifndef BLOSSOM_FACTORY_CAMERA_H
+#define BLOSSOM_FACTORY_CAMERA_H
 
 #include <entt/entt.hpp>
 #include "../components/transform.h"
 #include "../components/transform_matrix.h"
-#include "../components/perspective_camera.h"
+#include "../components/camera.h"
 #include "../components/view_projection_matrix.h"
 #include "../components/tags/active_camera.h"
 
 namespace blossom::factory
 {
-  class perspective_camera
+  class camera
   {
     public:
-      explicit perspective_camera(entt::registry& registry) 
+      explicit camera(entt::registry& registry) 
         : registry_(registry)
       {
         entity_    =  registry_.create();
-        registry_.emplace<component::perspective_camera>(entity_);
+        registry_.emplace<component::camera>(entity_);
         registry_.emplace<component::transform>(entity_);
         registry_.emplace<component::transform_matrix>(entity_);
         registry_.emplace<component::view_projection_matrix>(entity_);
       }
 
-      auto with_width(const uint16_t width) -> perspective_camera&
+      auto with_width(const uint16_t width) -> camera&
       {
-        registry_.get<component::perspective_camera>(entity_).width = width;
+        registry_.get<component::camera>(entity_).width = width;
         return *this;
       }
 
-      auto with_height(const uint16_t height) -> perspective_camera&
+      auto with_height(const uint16_t height) -> camera&
       {
-        registry_.get<component::perspective_camera>(entity_).height = height;
+        registry_.get<component::camera>(entity_).height = height;
         return *this;
       }
 
-      auto with_fov_y(float fov_y) -> perspective_camera&
+      auto with_fov_y(float fov_y) -> camera&
       {
-        registry_.get<component::perspective_camera>(entity_).fov_y = fov_y;
+        registry_.get<component::camera>(entity_).fov_y = fov_y;
         return *this;
       }
 
-      auto with_position(const glm::vec3& position) -> perspective_camera&
+      auto with_position(const glm::vec3& position) -> camera&
       {
         registry_.get<component::transform>(entity_).position = position;
         return *this;
       }
 
-      auto with_rotation(const glm::vec3& rotation) -> perspective_camera&
+      auto with_rotation(const glm::vec3& rotation) -> camera&
       {
         registry_.get<component::transform>(entity_).rotation = rotation;
         return *this;
       }
 
-      auto make_active() -> perspective_camera&
+      auto with_type(const component::camera::camera_type& type)
+      {
+        registry_.get<component::camera>(entity_).type = type;
+        return *this;
+      }
+
+      auto make_active() -> camera&
       {
         auto view = registry_.view<component::tag::active_camera>();
         registry_.remove<component::tag::active_camera>(view.begin(), view.end());
